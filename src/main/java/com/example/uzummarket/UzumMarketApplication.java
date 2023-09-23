b2bd17a50e2d863551f5395faef550d68b33a826
Xojiakbar Kamolov <xdasturchi777@gmail.com>
2023-09-23 12:48:49 +0500

    Start

---
+++ src/main/java/com/example/uzummarket/UzumMarketApplication.java
@@ -0,0 +1,12 @@
+package com.example.uzummarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzumMarketApplication {
+    public static void main(String[] args) {
+        System.out.println("Project is running...");
+        SpringApplication.run(UzumMarketApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/uzummarket/UzumMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uzummarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
